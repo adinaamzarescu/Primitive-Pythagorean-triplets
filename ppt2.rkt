13bc6026dfef6e88f85640ac20c97baa7232c2ad
Adina Amzărescu <73987572+adinaamzarescu@users.noreply.github.com>
2022-03-23 21:58:28 +0200

    Update ppt2.rkt

--- ppt2.rkt
+++ ppt2.rkt
@@ -1,7 +1,52 @@
 #lang racket
 
 (provide (all-defined-out))
+;; Același arbore de TPP obținut în etapa 1 prin aplicarea
+;; transformărilor T1, T2, T3 poate fi generat folosind 
+;; tupluri GH (Gopal-Hemachandra).
+;;
+;; Pentru o pereche oarecare (g, e), secvența GH este:
+;;    g, e, g + e, g + 2e, 2g + 3e, 3g + 5e ...
+;; Pentru (g, e) = (1, 1) obținem șirul lui Fibonacci.
+;;
+;; Primele 4 numere din secvență formează cvartetul GH:
+;;    (g, e, f, h) = (g, e, g + e, g + 2e)
+;;
+;; Pentru un asemenea cvartet (g, e, f, h), definim:
+;;    a = gh,   b = 2ef,   c = e^2 + f^2
+;; și putem demonstra că (a,b,c) este un triplet pitagoreic.
+;;
+;; (a,b,c) este chiar TPP, dacă adăugăm condițiile:
+;;    g, e, f, h prime între ele
+;;    g impar
+;; însă nu veți avea nevoie să faceți asemenea verificări,
+;; întrucât avem la dispoziție un algoritm care generează
+;; exclusiv TPP.
+;;
+;; Acest algoritm este foarte asemănător cu cel din etapa
+;; anterioară, cu următoarele diferențe:
+;;  - nodurile din arbore sunt cvartete, nu triplete
+;;    (din cvartet obținem un TPP conform formulelor)
+;;    (ex: (1,1,2,3) => (1*3,2*1*2,1^2+2^2) = (3,4,5))
+;;  - obținem următoarea generație de cvartete folosind 
+;;    trei transformări Q1, Q2, Q3 pentru cvartete, în loc
+;;    de T1, T2, T3 care lucrau cu triplete
+;; 
+;; Q1(g,e,f,h) = (h,e,h+e,h+2e)
+;; Q2(g,e,f,h) = (h,f,h+f,h+2f) 
+;; Q3(g,e,f,h) = (g,f,g+f,g+2f)
+;;
+;; Arborele rezultat arată astfel:
+;;
+;;                        (1,1,2,3)
+;;              ______________|______________
+;;             |              |              |
+;;         (3,1,4,5)      (3,2,5,7)      (1,2,3,5)
+;;       ______|______  ______|______  ______|______
+;;      |      |      ||      |      ||      |      |
+;;  (5,1,6,7) .........................................
 
+;; Definim funcțiile Q1, Q2, Q3:
 (define (Q1 g e f h) (list h e (+ h e) (+ h e e)))
 (define (Q2 g e f h) (list h f (+ h f) (+ h f f)))
 (define (Q3 g e f h) (list g f (+ g f) (+ g f f)))
@@ -10,6 +55,7 @@
 (define (FQ2 V) (apply Q2 V))
 (define (FQ3 V) (apply Q3 V))
 
+;; Vom refolosi matricile T1, T2, T3:
 (define T1 '((-1 2 2) (-2 1 2) (-2 2 3)))
 (define T2 '( (1 2 2)  (2 1 2)  (2 2 3)))
 (define T3 '((1 -2 2) (2 -1 2) (2 -2 3)))
@@ -27,6 +73,11 @@
   (map (λ(X) (dot-product X V)) M))
 
 
+; TODO
+; Aduceți aici (nu sunt necesare modificări) implementarea
+; funcției get-transformations de la etapa 1.
+; Această funcție nu este re-punctată de checker, însă este
+; necesară implementărilor ulterioare.
 (define (get-transformations n)
   (if (= n 1)
       '()
@@ -42,6 +93,24 @@
                  [else 2])))))
 
 
+; TODO
+; În etapa anterioară ați implementat o funcție care primea
+; o listă Ts de tipul celei întoarsă de get-transformations
+; și un triplet de start ppt și întorcea tripletul rezultat
+; în urma aplicării transformărilor din Ts asupra ppt.
+; Acum dorim să generalizăm acest proces, astfel încât să
+; putem reutiliza funcția atât pentru transformările de tip
+; T1, T2, T3, cât și pentru cele de tip Q1, Q2, Q3.
+; În acest scop operăm următoarele modificări:
+;  - primul parametru este o listă de funcții Fs
+;    (în loc de o listă numerică Ts)
+;  - al doilea parametru reprezintă un tuplu oarecare
+;    (aici modificarea este doar "cu numele", fără a schimba
+;    funcționalitatea, este responsabilitatea funcțiilor din
+;    Fs să primească parametri de tipul lui tuple)
+; Nu folosiți recursivitate explicită (ci funcționale).
+
+
 ; First of all foldl takes every function of the Fs list
 ; Then the λ function applies that specific function on tuple
 ; and in the end the result is stored in tuple
@@ -49,6 +118,27 @@
   (foldl (λ(given-function tuple) (given-function tuple)) tuple Fs))
 
 
+; TODO
+; Tot în spiritul abstractizării, veți defini o nouă funcție
+; get-nth-tuple, care calculează al n-lea tuplu din arbore. 
+; Această funcție va putea fi folosită:
+;  - și pentru arborele de triplete (caz în care plecăm de la
+;    (3,4,5) și avansăm via T1, T2, T3)
+;  - și pentru arborele de cvartete (caz în care plecăm de la
+;    (1,1,2,3) și avansăm via Q1, Q2, Q3)
+; Rezultă că, în afară de parametrul n, funcția va trebui să
+; primească un tuplu de start și 3 funcții de transformare a
+; tuplurilor.
+; Definiți get-nth-tuple astfel încât să o puteți reutiliza
+; cu minim de efort pentru a defini funcțiile următoare:
+;    get-nth-ppt-from-matrix-transformations
+;    get-nth-quadruple
+; (Hint: funcții curry)
+; În define-ul de mai jos nu am precizat parametrii funcției
+; get-nth-tuple pentru ca voi înșivă să decideți care este
+; modul optim în care funcția să își primească parametrii.
+; Din acest motiv checker-ul nu testează separat această funcție,
+; dar asistentul va observa dacă implementarea respectă cerința.
 (define (get-nth-tuple tuple)
   (λ(f)
     (apply-functional-transformations f tuple))) 
@@ -62,6 +152,10 @@
       (cond [(= (car transformations) 1) (get-multiply-order (cdr transformations) L (cons (car L) acc))]
             [(= (car transformations) 2) (get-multiply-order (cdr transformations) L (cons (car(cdr L)) acc))]
             [(= (car transformations) 3) (get-multiply-order (cdr transformations) L (cons (car(cdr(cdr L))) acc))])))
+; TODO
+; Din get-nth-tuple, obțineți în cel mai succint mod posibil
+; (hint: aplicare parțială) o funcție care calculează al n-lea
+; TPP din arbore, folosind transformările pe triplete.
 
 ; get-multiply-order will receive a list of transformations (get-transformations n), the 3 matrix T1 T2 T3 and will return a list
 ; of the order in which they should be multiplied
@@ -70,12 +164,19 @@
   ((get-nth-tuple '(3 4 5)) (map (curry multiply) (get-multiply-order (get-transformations n) (list T1 T2 T3) '()))))
 
 
+; TODO
+; Din get-nth-tuple, obțineți în cel mai succint mod posibil 
+; (hint: aplicare parțială) o funcție care calculează al n-lea 
+; cvartet din arbore, folosind transformările pe cvartete.
 
 ; This one is similar but in this case the list is a list of functions, FQ1 FQ2 FQ3
 (define (get-nth-quadruple n)
   ((get-nth-tuple '(1 1 2 3)) (get-multiply-order (get-transformations n) (list FQ1 FQ2 FQ3) '())))
 
 
+; TODO
+; Folosiți rezultatul întors de get-nth-quadruple pentru a 
+; obține al n-lea TPP din arbore.
 ; a = gh,   b = 2ef,   c = e^2 + f^2
 (define (n-thTTP g e f h)
   (list (* g h) (* 2 e f) (+ (* e e) (* f f))))
